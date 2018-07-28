945cf4a4f265c0d30ed5962550f813456b46167f
divmohan <divya.mohan310@gmail.com>
2018-07-28 16:17:25 +0530

    added support for Periodic watering, on command watering, timelapse images

--- SmartWatering.py
+++ SmartWatering.py
@@ -1,16 +1,22 @@
 #Import the necessary libraries
 import RPi.GPIO as GPIO
-import sys
 
+import threading
+import telepot
 import time
+
+import sys
 import subprocess
+import os
 from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
+
 #Setup pin 18 as an output
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 #This function turns the valve on and off in 10 sec. intervals. 
+
 
 def valveOff(Pin):
         GPIO.output(18, GPIO.HIGH)
@@ -20,46 +26,111 @@ def valveOn(Pin):
         GPIO.output(18, GPIO.LOW)
         print("GPIO LOW (off), valve should be on")
         
-def takeVideo():
-        print("In takeVideo()")
-	cmd = "raspivid -o WateringProof.h264 -t 10000"
+
+def takeVideo(videoFile):
+        print("Save the video",videoFile);
+        #cmd = "raspivid -o WateringProof.h264 -t 10000"
+        cmd = "raspivid -o " + videoFile + " -t 10000"
 	subprocess.call(cmd, shell=True)
 
-def waterPlants():
+def sendVideo(videoFile):
+        chatId='311849016'
+        document = open(videoFile, 'rb')
+        bot.sendDocument(chatId, document)
+        document.close()
+        #delete the video file
+        os.remove(videoFile);
+
+
+def waterPlants(  ):
+    with lock:  
+        print "Acquired lock:start watering at:" + datetime.now().strftime('%H:%M:%S') 
         valveOn(18);
         time.sleep(5);
+
         #start camera and take video
-        takeVideo()
+        fileTimestamp = datetime.now().strftime('%Y-%m-%d%H:%M:%S') 
+        videoFile     = fileTimestamp + '.h264'
+        takeVideo(videoFile)
+	
         #time.sleep(10)
         valveOff(18);
 
+        #send video to phone
+        sendVideo(videoFile)
 
-#Check function to see if solenoid is off or on and alert if its not working well
-#def checkAndNotify():
+        print "Video sent at " + datetime.now().strftime('%H:%M:%S')  
 
 
-print("Arguments:",sys.argv[1]);
+class myThread (threading.Thread):
+   def __init__(self, threadID, name):
+      threading.Thread.__init__(self)
+      self.threadID = threadID
+      self.name = name
+   def run(self):
+      print "Starting " + self.name
+      waterDaily(self.name)
+      print "Exiting " + self.name
 
-#Switch on the valve 20 seconds and then switch off
-#Get time of day, if evening 5, water the plants
-wateringTime= '22'
+lock = threading.Lock()
 
-currentTime = datetime.now().strftime('%H')
-print("Current time:",currentTime)
+def waterDaily(threadName):
+   #Switch on the valve 20 seconds and then switch off
+   #Get time of day, if evening 5, water the plants
+   wateringTime= '10:00'   
 
-print("Water time:",wateringTime)
+   while True:
+      currentTime = datetime.now().strftime('%H:%M')
+      print("Current time:",currentTime)
+      print("Water time:",wateringTime)
+      if (currentTime == wateringTime):         
+        waterPlants();
+        time.sleep(86280);  #23 hrs in seconds
+                 			
+      else:
+   	  print("Its not yet time to water")
+          time.sleep(10);
 
-if (currentTime == wateringTime):
-        if( 'BOC' == str(sys.argv[1]) ):
-            print("Based on climate")
+      
 
-        elif('BOCS' == str(sys.argv[1])):
-            print("Based on climate and sensor readings");
 
+def handle(msg):
+        chat_id = msg['chat']['id']
+        command = msg['text']
+
+        words = command.split()
+        str = ' '.join(words[1:999])
+
+        if words[0] == 'Waterplants':
+                print "Call function to water plants" 
+                waterPlants();
+                # os.system("/home/pi/pi/dev/youtube-music/play-from-youtube.sh %s &" % str)
         else:
-            print("Water the plants");
-            waterPlants();
-          
-else:
-   print("Its not yet time to water")
-#GPIO.cleanup()
+                bot.sendMessage(chat_id, "Invalid Message")
+
+
+def periodicImageCapture():
+     imageTimestamp = datetime.now().strftime('%Y-%m-%d%H:%M:%S') 
+     imageFile      = 'images/'+ imageTimestamp + '.jpg'
+     with lock:
+         cmd = "raspistill -o " + imageFile 
+         subprocess.call(cmd, shell=True)
+
+
+# Create new threads
+thread1 = myThread(1, "DailyThread")
+
+# Start new Threads
+thread1.start()
+
+
+bot = telepot.Bot('322060273:AAHjszgt4w95TKgGMtpAN-I1Nbv7VA7-5Lg')
+bot.message_loop(handle)
+
+while 1:
+        periodicImageCapture()
+        time.sleep(7200)  #every 2 hours
+
+
+thread1.join()
+print "Exiting Main Thread"
